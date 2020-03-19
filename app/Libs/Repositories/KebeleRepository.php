c0255f2691f342be0a70947a0a08504a9f3eacb6
bengeos <george.beng@gmail.com>
2020-03-19 08:44:35 +0300

    add kebele repository ctl

---
+++ app/Libs/Repositories/KebeleRepository.php
@@ -0,0 +1,136 @@
+<?php
+
+
+namespace App\Libs\Repositories;
+
+
+use App\Libs\Interfaces\DefaultInterface;
+use App\Models\Kebele;
+use App\Models\SubCity;
+
+class KebeleRepository extends DefaultRepository implements DefaultInterface
+{
+
+    /**
+     * KebeleRepository constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function getItem($id, $queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        return Kebele::where('id', '=', $id)
+            ->where(function ($query) use ($queryData) {
+                $this->queryBuilder($query, $queryData);
+            })
+            ->first();
+    }
+
+    public function getItemBy($queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        return Kebele::where(function ($query) use ($queryData) {
+            $this->queryBuilder($query, $queryData);
+        })
+            ->first();
+    }
+
+    public function getAll($queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        return Kebele::where(function ($query) use ($queryData) {
+            $this->queryBuilder($query, $queryData);
+        })
+            ->get();
+    }
+
+    public function getAllPaginated($pagination_size = 10, $queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        return Kebele::where(function ($query) use ($queryData) {
+            $this->queryBuilder($query, $queryData);
+        })
+            ->paginate($pagination_size);
+    }
+
+    public function addNew($inputData)
+    {
+        $newKebele = new Kebele();
+        $newKebele->sub_city_id = isset($inputData['sub_city_id']) ? $inputData['sub_city_id'] : null;
+        $newKebele->name = isset($inputData['name']) ? $inputData['name'] : null;
+        $newKebele->latitude = isset($inputData['latitude']) ? $inputData['latitude'] : null;
+        $newKebele->longitude = isset($inputData['longitude']) ? $inputData['longitude'] : null;
+        $newKebele->description = isset($inputData['description']) ? $inputData['description'] : null;
+        $newKebele->save();
+        return $newKebele;
+    }
+
+    public function updateItem($id, $updateData, $queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        $queryData['id'] = $id;
+        return Kebele::where(function ($query) use ($queryData) {
+            if ($queryData) {
+                $this->queryBuilder($query, $queryData);
+            }
+        }
+        )->update($updateData);
+    }
+
+    public function updateItemBy($queryData, $updateData)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        return Kebele::where(function ($query) use ($queryData) {
+            if ($queryData) {
+                $this->queryBuilder($query, $queryData);
+            }
+        }
+        )->update($updateData);
+    }
+
+    public function deleteItem($id, $queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        return Kebele::where('id', '=', $id)->where(function ($query) use ($queryData) {
+            if ($queryData) {
+                $this->queryBuilder($query, $queryData);
+            }
+        }
+        )->delete();
+    }
+
+    public function deleteItemBy($queryData = null)
+    {
+        if ($queryData == null) {
+            $queryData = array();
+        }
+        $kebelesForDelete = SubCity::where(function ($query) use ($queryData) {
+            if ($queryData) {
+                $this->queryBuilder($query, $queryData);
+            }
+        }
+        )->get();
+        foreach ($kebelesForDelete as $kebele) {
+            if ($kebele instanceof Kebele) {
+                $kebele->delete();
+            }
+        }
+        return true;
+    }
+}
